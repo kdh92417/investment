50724fca8b017bb7f291946b4de78855133f2243
kdh92417 <kdh92417@gmail.com>
2022-09-17 22:42:21 +0900

    :sparkles: Feat: 투자 API를 위한 모델링 작성
    - 투자, 종목, 유저, 계좌, 보유종목 모델 작성 및 생성

---
+++ backend/api/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+
+
+class Account(models.Model):
+    """계좌 모델"""
+
+    account_number = models.CharField("계좌번호", max_length=30, primary_key=True)
+    account_name = models.CharField("계좌명", max_length=45, null=False)
+    total_assets = models.DecimalField(
+        "계좌 총 자산", max_digits=17, decimal_places=2, default=0, null=True, blank=True
+    )
+
+    class Meta:
+        db_table = "accounts"
+
+    def __str__(self):
+        return self.account_name
+
+
+class User(models.Model):
+    """유저 모델"""
+
+    account = models.OneToOneField(
+        Account,
+        on_delete=models.SET_DEFAULT,
+        related_name="user",
+        primary_key=True,
+        default="0",
+    )
+    name = models.CharField(max_length=45, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.name
+
+
+class Investment(models.Model):
+    """투자 모델"""
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name="investment"
+    )
+    brokerage = models.CharField("증권사", max_length=45)
+    principal = models.DecimalField(
+        "투자 원금", max_digits=17, decimal_places=2, null=True, blank=True
+    )
+
+    class Meta:
+        db_table = "investments"
+
+
+class Holding(models.Model):
+    """종목 모델"""
+
+    name = models.CharField("종목명", max_length=45, unique=True, null=False, blank=False)
+    isin = models.CharField("ISIN", max_length=20, unique=True, null=False, blank=False)
+    asset_group = models.CharField("자산그룹", max_length=45)
+
+    class Meta:
+        db_table = "holdings"
+
+    def __str__(self):
+        return self.name
+
+
+class UserHolding(models.Model):
+    """유저 보유종목 모델"""
+
+    holding = models.ForeignKey(Holding, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="user_holdings"
+    )
+    quantity = models.IntegerField("보유 종목 수량", default=1)
+    current_price = models.FloatField("현재가", null=True, blank=True)
+
+    class Meta:
+        db_table = "users_holdings"
