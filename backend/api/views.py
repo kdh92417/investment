c6e18cbda3080c9c9d1408ebbaadee71cb81f341
kdh92417 <kdh92417@gmail.com>
2022-09-20 23:11:54 +0900

    :sparkles: Feat: 입금거래 API 추가
    입금거래 인증 API 및 자산업데이트 API 추가

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,20 @@
+import jwt
+
+from datetime import datetime, timedelta
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import User, Investment, UserHolding
+from api.models import User, Investment, UserHolding, DepositLog
 from api.serializers import (
     InvestmentViewSerializer,
     InvestmentDetailViewSerializer,
     UserHoldingViewSerializer,
+    DepositLogSerializer,
+    AssetSerializer,
 )
+from backend.settings.base import SECRET_KEY, SIGNATURE_ALGORITHM
 
 
 class InvestmentView(APIView):
@@ -36,5 +43,32 @@ class UserHoldingView(APIView):
     def get(self, request, pk):
         queryset = UserHolding.objects.filter(user=User.objects.get(id=pk))
         serializer = UserHoldingViewSerializer(queryset, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class InvestmentDeposit(APIView):
+    """입금 거래 API"""
+
+    def post(self, request):
+        payload = {**request.data, "exp": datetime.utcnow() + timedelta(days=3)}
+        signature = jwt.encode(
+            payload, SECRET_KEY, algorithm=SIGNATURE_ALGORITHM
+        )  # jwt를 이용하여 해싱
+        serializer = DepositLogSerializer(data={**payload, "signature": signature})
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_201_CREATED,
+        )
+
+    def put(self, request):
+        deposit_log = DepositLog.objects.get(id=request.data.get("transfer_identifier"))
+        serializer = AssetSerializer(deposit_log, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
