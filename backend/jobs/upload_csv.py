e4a9672f456e2cf2a08af91dda7b67bc99ac8f5e
kdh92417 <kdh92417@gmail.com>
2022-09-21 13:58:25 +0900

    :sparkles: csv upload 스케쥴러 추가
    - apscheduler를 이용한 CSV업로드 스케쥴링 추가

--- backend/jobs/upload_csv.py
+++ backend/jobs/upload_csv.py
@@ -1,20 +1,26 @@
 import logging
 import os
-import django
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
 from django.core.exceptions import ValidationError
 
 import csv
 
 from django.db import transaction
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.local")
-django.setup()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.local")
+# django.setup()
 
 from api.models import Holding, User, Account, Investment, UserHolding
+from backend.settings.base import TIME_ZONE
 
-CSV_ACCOUNT_ASSET = "./csv/account_asset_info_set.csv"
-CSV_ACCOUNT_BASIC = "./csv/account_basic_info_set.csv"
-CSV_ASSET_GROUP = "./csv/asset_group_info_set.csv"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+CSV_ACCOUNT_ASSET = os.path.join(dir_path, "csv/account_asset_info_set.csv")
+CSV_ACCOUNT_BASIC = os.path.join(dir_path, "csv/account_basic_info_set.csv")
+CSV_ASSET_GROUP = os.path.join(dir_path, "csv/asset_group_info_set.csv")
 
 
 def upload_asset_group_info(csv_asset_group):
@@ -207,8 +213,11 @@ def calculate_account_total_asset():
 
 
 def execute_csv_uploader():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    filename = os.path.join(dir_path, "test_log.log")
+    """
+    Execute all csv uploader function
+    :return:
+    """
+    filename = os.path.join(dir_path, "../test_log.log")
 
     # Logger
     logger = logging.getLogger(__name__)
@@ -220,11 +229,30 @@ def execute_csv_uploader():
         logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     )
     logger.addHandler(file_handler)
-    logger.info("start..")
-    print("start")
+    logger.info("csv upload start..")
+
     upload_asset_group_info(CSV_ASSET_GROUP)
     upload_asset_info(CSV_ACCOUNT_ASSET)
     upload_asset_basic(CSV_ACCOUNT_BASIC)
     calculate_account_total_asset()
-    print("end")
-    logger.info("end..")
+
+    logger.info("csv upload start.. end")
+
+
+def start():
+    """
+    스케쥴러 실행
+    - 실행 함수 : execute_csv_uploader
+    - 주기 : 매일 06시 실행
+    :return:
+    """
+    scheduler = BackgroundScheduler(timezone=TIME_ZONE)
+    scheduler.add_job(
+        execute_csv_uploader,
+        id="execute_csv_uploader",
+        trigger=CronTrigger(hour="6"),
+        max_instances=1,
+        replace_existing=True,
+        coalesce=True,
+    )
+    scheduler.start()
